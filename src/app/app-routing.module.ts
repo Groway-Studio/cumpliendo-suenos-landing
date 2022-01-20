16661a98c701672d2c8631d929cf930de58dc990
cnahuina <cnahuina@gmail.com>
2022-01-20 08:21:45 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'heroes', component: HeroesComponent },
+  // { path: 'buscar/:termino', component: BuscadorComponent },
+  // { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full',redirectTo: '' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+}
